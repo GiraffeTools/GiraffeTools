02d8ca95cc2ab00fcf81acdcde0553382b1c1278
Tim van Mourik <TimVanMourik@users.noreply.github.com>
2018-03-21 17:11:54 +0100

    porcupine settings update

---
+++ porcupine/settings/local.py
@@ -0,0 +1,7 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 's9&vp1jq1yzr!1c_temg#v_)j-a)i5+@vbsekmi6pbjl4l1&u@'
