54a83ccf7d8b60d807645da12d6fb824c256daed
Tim van Mourik <TimVanMourik@users.noreply.github.com>
2018-03-22 14:28:01 +0100

    rel path

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@
         },
         output: {
             path: path.resolve(__dirname),
-            filename: '/static/bundle/[name].js'
+            filename: './static/bundle/[name].js'
         },
 
    plugins: [
