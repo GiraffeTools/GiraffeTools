1149f1e7caf12df4309b732ee2233dbb39e6b3fa
Tim van Mourik <TimVanMourik@users.noreply.github.com>
2018-03-22 14:01:36 +0100

    static file update

--- app/settings/base.py
+++ app/settings/base.py
@@ -15,13 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-STATICFILES_DIRS = (
-    os.path.normpath(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'static')),
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-)
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
@@ -129,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'porcupine/static/lib')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, '../node_modules/bootstrap/dist'),
     os.path.join(BASE_DIR, '../node_modules/jquery/dist'),
