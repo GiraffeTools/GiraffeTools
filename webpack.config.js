d0378203979d3eeeb8a87d09f7c6d073329b38bd
Tim van Mourik <TimVanMourik@users.noreply.github.com>
2018-03-22 17:46:19 +0100

    nope... webpack output to different location?

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@
         },
         output: {
             path: path.resolve(__dirname),
-            filename: './staticfiles/bundle/[name].js'
+            filename: '[name]/static/bundle/[name].js'
         },
 
    plugins: [
