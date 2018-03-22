c00c40417cf4eeccbe430001d7c6ea0ec3a03eee
Tim van Mourik <TimVanMourik@users.noreply.github.com>
2018-03-22 13:46:48 +0100

    static path change

--- app/settings/base.py
+++ app/settings/base.py
@@ -128,7 +128,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/static/lib/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'porcupine/static/lib')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, '../node_modules/bootstrap/dist'),
