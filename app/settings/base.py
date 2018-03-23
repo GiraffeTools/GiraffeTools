127b614cf06975afcfa2506affa4720abb3ecea1
Tim van Mourik <TimVanMourik@users.noreply.github.com>
2018-03-23 15:34:02 +0100

    bloody middleware...

--- app/settings/base.py
+++ app/settings/base.py
@@ -41,7 +41,7 @@ MIDDLEWARE_CLASSES = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    # 'django.contrib.auth.middleware.SessionAuthenticationMiddleware', #This one screws up static files
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
