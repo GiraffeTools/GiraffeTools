f882a6353009eb55301f4921ed88c091279c623a
Tim van Mourik <TimVanMourik@users.noreply.github.com>
2018-03-23 12:24:05 +0100

    django heroku settings

--- app/settings/base.py
+++ app/settings/base.py
@@ -134,3 +134,7 @@ STATICFILES_DIRS = (
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
