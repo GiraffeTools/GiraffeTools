d5abf47751598576646ceaa902dc0af2598c9b40
Mihir Patel <ee.mihir.patel@gmail.com>
2018-04-10 22:42:23 -0400

    raise 404 when invalid github repo or branch is specified for giraffe routes

--- app/giraffe/views.py
+++ app/giraffe/views.py
@@ -1,6 +1,7 @@
 from django.template.response import TemplateResponse
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from .utils.GiraffeConfig import GiraffeConfig
+from .utils.GithubValidation import isValidSetOfGithubDetails
 
 import pydash, urllib.error, urllib.request, yaml
 
@@ -11,10 +12,12 @@ def index(request):
 def project(request, ghuser='', ghrepo='', ghbranch='master'):
     """Recognise that this is a github repository that contains a GIRAFFE.yml file"""
 
+    if not isValidSetOfGithubDetails(ghuser, ghrepo, ghbranch):
+        raise Http404;
+
     try:
         giraffeConfig = GiraffeConfig(ghuser, ghrepo, ghbranch)
     except urllib.error.HTTPError:
-        # @TODO for issue #3/#4, Make distinction between 'this is no giraffe repo' and 'this isn't a repo at all'
         giraffeConfig = None
 
     params = {
@@ -28,6 +31,10 @@ def project(request, ghuser='', ghrepo='', ghbranch='master'):
 
 def projectTool(request, ghuser='', ghrepo='', ghbranch='master', toolName=''):
     """Recognise that this is a github repository with GIRAFFE.yml defining this tool"""
+
+    if not isValidSetOfGithubDetails(ghuser, ghrepo, ghbranch):
+        raise Http404;
+
     giraffeConfig = GiraffeConfig(ghuser, ghrepo, ghbranch)
     filename = giraffeConfig.getToolAttribute(toolName, 'file')[0]
     fileData = giraffeConfig.getToolFileData(toolName)
