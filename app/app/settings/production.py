3a2c5ef827fa1081fc613323f89be093db1bc462
Tim van Mourik <TimVanMourik@users.noreply.github.com>
2018-04-13 12:04:13 +0200

    production settings updated

--- app/app/settings/production.py
+++ app/app/settings/production.py
@@ -9,8 +9,10 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # optional: only needed if you run the github management commands
 # Login/logout functionality will not work without defining these fields.
 # Setup instructions: https://github.com/gitcoinco/web#setup-github-oauth2-app-integration
-GITHUB_CLIENT_ID = 'TODO'
-GITHUB_CLIENT_SECRET = 'TODO'
-GITHUB_API_USER = 'TODO'
-GITHUB_API_TOKEN = 'TODO'
-GITHUB_APP_NAME = 'TODO'
+GITHUB_CLIENT_ID     = os.environ['GITHUB_CLIENT_ID']
+GITHUB_CLIENT_SECRET = os.environ['GITHUB_CLIENT_SECRET']
+GITHUB_API_USER      = os.environ['GITHUB_API_USER']
+GITHUB_API_TOKEN     = os.environ['GITHUB_API_TOKEN']
+GITHUB_APP_NAME      = os.environ['GITHUB_APP_NAME']
+
+BASE_URL = 'https://www.giraffe.tools/'
