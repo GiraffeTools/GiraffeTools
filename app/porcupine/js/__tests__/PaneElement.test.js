1cd53ed74751a09050073ae158244da725d67512
Agnivesh Patel <agniveshpatel@gmail.com>
2018-05-01 15:15:11 +0530

    test for paneElement

--- app/porcupine/js/__tests__/PaneElement.test.js
+++ app/porcupine/js/__tests__/PaneElement.test.js
@@ -1,19 +1,53 @@
-import React from 'react'
+import React, { Component } from 'react';
+import ReactTestUtils from 'react-dom/test-utils';
+import TestBackend from 'react-dnd-test-backend';
+import { DragDropContext } from 'react-dnd';
+import {shallow, mount} from "enzyme";
 import PaneElement from "../paneElement";
-import {shallow} from "enzyme";
+
+/**
+ * Wraps a component into a DragDropContext that uses the TestBackend.
+ */
+function wrapInTestContext(DecoratedComponent) {
+  return DragDropContext(TestBackend)(
+    class TestContextContainer extends Component {
+      render() {
+        return <DecoratedComponent {...this.props} />;
+      }
+    }
+  );
+}
 
 describe('<PaneElement>', () => {
-    it('wraps children in a draggable div', () => {
-        // const comp = shallow(
-        //     <PaneElement category={[]} id="1">
-        //         {"title"}
-        //     </PaneElement>
-        // )
+  // Render with the test context that uses the test backend
+    const BoxContext = wrapInTestContext(PaneElement);
+    const root = mount(
+        <BoxContext
+          key = "testElement"
+          category = {['test', 'category']}
+          id  = "test.Element"
+        >testElement</BoxContext>
+    );
+    //
+    it('can be tested with the testing backend', () => {
 
-        // @TODO to make this work, please bring in the snapshot that was removed in commit #152
-        // The reason it was deleted instead of commented out is that `npm test` complains about unused snapshots
+      // Test that the opacity is 1
+      let div = root.find('div.btn').get(0)
+      expect(div.props.style.opacity).toEqual(1)
 
-        // expect(comp).toMatchSnapshot()
-      expect("").toMatch("") // mock line to make the test pass
-    })
+    });
+    // it('can be draged', () => {
+
+    //   // Obtain a reference to the backend
+    //   const backend = root.instance().getManager().getBackend();
+
+    //   // Find the drag source ID and use it to simulate the dragging operation
+    //   backend.simulateBeginDrag([root.find(PaneElement).instance().getHandlerId()]);
+    //   console.log(root.find(PaneElement).instance())
+    //   // Verify that the div changed its opacity
+    //   let div = root.find('div.btn').get(0)
+    //   expect(div.props.style.opacity).toEqual(0.5);
+
+    // });
 })
+
