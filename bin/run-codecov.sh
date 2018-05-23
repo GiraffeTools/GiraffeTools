3f6ec150034611ec25a14054a25d96e074f8f4f0
Aditya Jha <aditya.jha@indifi.com>
2018-05-23 16:23:23 +0530

    reverse order of js and python tests

--- bin/run-codecov.sh
+++ bin/run-codecov.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-# python tests
-coverage run ./manage.py test --noinput --settings=app.settings.local
-bash <(curl -s https://codecov.io/bash) -cF python
-
 # javascript tests
 cd ..
 npm test
 bash <(curl -s https://codecov.io/bash) -cF javascript
+
+# python tests
+cd app
+coverage run ./manage.py test --noinput --settings=app.settings.local
+bash <(curl -s https://codecov.io/bash) -cF python
+
