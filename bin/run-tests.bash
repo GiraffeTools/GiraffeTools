a73ee585faaac05cb04e67b8bc71c922f620ff70
Tim van Mourik <TimVanMourik@users.noreply.github.com>
2018-05-23 15:12:51 +0200

    merge codecov update with traivs dev

--- bin/run-tests.bash
+++ bin/run-tests.bash
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-cd app
-python manage.py test
+# javascript tests
 cd ..
 npm test
+bash <(curl -s https://codecov.io/bash) -cF javascript
+
+# python tests
+cd app
+coverage run ./manage.py test --noinput --settings=app.settings.local
+bash <(curl -s https://codecov.io/bash) -cF python
