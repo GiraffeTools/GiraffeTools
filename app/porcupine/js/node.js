c49a702d277f852cc8ca6ebdbef499ee28f2c47f
Agnivesh Patel <agniveshpatel@gmail.com>
2018-05-27 13:08:12 +0530

    connect ports

--- app/porcupine/js/node.js
+++ app/porcupine/js/node.js
@@ -4,8 +4,61 @@ import React from 'react'
 class Node extends React.Component {
   constructor(props) {
     super(props);
+    this.connectPort   = this.connectPort.bind(this);
+    this.connect       = this.connect.bind(this);
   }
 
+  connectPort(e) {
+    e.stopPropagation()
+    this.connect(e.target)
+  }
+  connect(el) {
+    $(el).off('click')
+    var s = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    var l = document.createElementNS("http://www.w3.org/2000/svg", "line");
+    l.setAttribute("x1", "4");
+    l.setAttribute("y1", "4");
+    l.setAttribute("stroke", "black");
+    el.appendChild(s);
+    s.appendChild(l);
+    var xi=s.getClientRects()[0].x
+    var yi=s.getClientRects()[0].y
+    l.setAttribute("x2", 4);
+    l.setAttribute("y2", 4);
+        var that=this
+        console.log(this)
+    $('#zoomContainer').on('click', function(e) {
+      if (e.target.classList[0]==="node__port--input") {
+        // x=e.pageX-xi
+        // y=e.pageY-yi
+        var x, y
+        ({x,y}=e.target.getClientRects()[0])
+        x=x-xi+4
+        y=y-yi+4
+        console.log(e.target.getClientRects()[0],x,y)
+        l.setAttribute("x2", x);
+        l.setAttribute("y2", y);
+        $(el).on('click', (e)=>{
+          e.stopPropagation()
+          that.connect(el)
+        })
+      } else{
+        el.removeChild(s)
+        $(el).on('click', (e)=>{
+          e.stopPropagation()
+          that.connect(el)
+        })
+      }
+      $('#zoomContainer').off('mousemove')
+      $('#zoomContainer').off('click')
+    })
+    $('#zoomContainer').on('mousemove', function(e) {
+      var x=e.pageX-xi
+      var y=e.pageY-yi
+      l.setAttribute("x2", x);
+      l.setAttribute("y2", y);
+    })
+  }
   render() {
     const { x, y, colour, class: classname, id, type, click, ports } = this.props;
     const visiblePorts = ports.filter(port => port.visible);
@@ -30,17 +83,18 @@ class Node extends React.Component {
               <ul>
                 {
                   visiblePorts.map((port, index) => {
-                    let portClassName = '';
+                    let portElement = '';
                     if (port.input) {
-                      portClassName = 'node__port--input';
+                      portElement = <span  className='node__port--input'/>
                     } else if (port.output) {
-                      portClassName = 'node__port--output';
+                      portElement = <span onClick={(event) => this.connectPort(event)} className='node__port--output'/>
                     }
 
                     return (
                       <li key={index}>
                         <div className='node__port'>
-                          {port.name}<span className={portClassName}/>
+                          {port.name}
+                          {portElement}
                         </div>
                       </li>
                     )
