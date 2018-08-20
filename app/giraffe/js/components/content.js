bfab2a65aec986ab45d4d4fa023fe8c07199ea2b
TimVanMourik <timvanmourik@gmail.com>
2018-08-20 23:44:17 +0200

    Placeholders for new homepage

--- app/giraffe/js/components/content.js
+++ app/giraffe/js/components/content.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import Hero from "./hero.js";
+
 
 class Content extends React.Component {
   constructor(props) {
@@ -11,41 +13,44 @@ class Content extends React.Component {
 
     return (
       <div>
-        <div class="container-fluid base">
-          <div class="col text-center">
+        <Hero />
+
+
+        <div className="container-fluid base">
+          <div className="col text-center">
             <h1>Giraffe</h1>
-            <h4 class="">Graphical Interface for Reproducible Analysis For workFlow Experiments</h4>
+            <h4 className="">Graphical Interface for Reproducible Analysis For workFlow Experiments</h4>
             Giraffe tools are applications that make it easier to analyse all kinds of data!
           </div>
           <div>
           </div>
         </div>
 
-        <div class="container-fluid mt-3 ">
-          <div class="col text-center">
+        <div className="container-fluid mt-3 ">
+          <div className="col text-center">
             <h1>Porcupine</h1>
-            <h4 class="">PORcupine Creates Ur PipelINE</h4>
+            <h4 className="">PORcupine Creates Ur PipelINE</h4>
             With Porcupine, you can visually build your pipeline!
           </div>
-          <div class="col text-center">
-            <a class="btn btn-lg btn-warning" role="button" href="/porcupine">Get started!</a>
+          <div className="col text-center">
+            <a className="btn btn-lg btn-warning" role="button" href="/porcupine">Get started!</a>
           </div>
         </div>
 
-        <div class="container-fluid mt-3 ">
-          <div class="col text-center">
+        <div className="container-fluid mt-3 ">
+          <div className="col text-center">
             <h1>Armadillo</h1>
-            <h4 class="">Augmented Reality [madillo]</h4>
+            <h4 className="">Augmented Reality [madillo]</h4>
             Augmented Reality brain images!
           </div>
-          <div class="col text-center">
-            <a class="btn btn-lg btn-warning" role="button" href="https://armadillo-brain.herokuapp.com">Get started!</a>
+          <div className="col text-center">
+            <a className="btn btn-lg btn-warning" role="button" href="https://armadillo-brain.herokuapp.com">Get started!</a>
           </div>
         </div>
 
-        <div class="container right">
+        <div className="container right">
           <h3>All contributors</h3>
-            <div class="card mb-12 text-center">
+            <div className="card mb-12 text-center">
               <div id="contributors">
             </div>
           </div>
@@ -53,10 +58,10 @@ class Content extends React.Component {
 
         <br />
 
-        <div class="container fluid mt-3">
-          <div class="col text-center">
+        <div className="container fluid mt-3">
+          <div className="col text-center">
             <a href="https://github.com/TimVanMourik/GiraffeTools" target="_blank">
-              <img src="{% static 'img/gh-icon.png' %}" width="4%"/>
+              {/* <img src="{% static 'img/gh-icon.png' %}" width="4%"/> */}
             </a>
             <h6>2018</h6>
           </div>
---
+++ app/giraffe/js/components/hero.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+
+const Hero = () => (
+  <div>
+  </div>
+);
+
+export default Hero;
