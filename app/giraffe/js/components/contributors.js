62b8c4c0b2986230d26e4d2c669860d754b975b5
Tim van Mourik <TimVanMourik@users.noreply.github.com>
2018-08-22 17:17:10 +0200

    contributor section to react

--- app/giraffe/js/components/contributors.js
+++ app/giraffe/js/components/contributors.js
@@ -1,30 +1,51 @@
 import React from "react";
 
-const Contributors = () => (
-  <div className="container right">
-    <h3>All contributors</h3>
-    <div className="card mb-12 text-center">
-      <div id="contributors" />
-    </div>
-  </div>
-);
+const Contributor = (contributor) => {
+  console.log(contributor);
+  return (
+    <li>
+      <img className="avatar-image" src={contributor.avatar_url} /><br />
+      <a href={contributor.html_url} target="_blank"> <b>@{contributor.login}</b></a>
+    </li>
+  )
+};
+
+class Contributors extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      contributors: null,
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://api.github.com/repos/TimVanMourik/GiraffeTools/contributors")
+      .then(response => response.json())
+      .then(contributors => this.setState({ contributors }));
+  }
+
+  render() {
+    const { contributors } = this.state;
+
+    return (
+      <div className="container right">
+        <h3>All contributors</h3>
+        <div className="card mb-12 text-center">
+          <div id="contributors" />
+            <ul className="contributor-list">
+            {
+              contributors && contributors.map(contributor => (
+                <Contributor
+                  key={contributor.id}
+                  {...contributor}
+                />
+              ))
+            }
+          </ul>
+        </div>
+      </div>
+    )
+  }
+};
 
 export default Contributors;
-
-
-// <script>
-// $.getJSON( "https://api.github.com/repos/TimVanMourik/GiraffeTools/contributors", function (data) {
-//   var users = [];
-//   $.each(data, function(key, val) {
-//     users.push( `<li>
-//                     <img class="avatar-image" src=${val.avatar_url} /><br />
-//                     <a href=${val.html_url} target=_blank> <b>@${val.login}</b></a>
-//                 </li>`);
-//   });
-//
-//   $( "<ul/>", {
-//     "class": "contributor-list",
-//     html: users.join( "" )
-//   }).appendTo( "#contributors" );
-// });
-// </script>
