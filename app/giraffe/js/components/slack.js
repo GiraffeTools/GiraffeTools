22364dd42b7c7c9bba2f1180eef57f93c73d9943
Tim van Mourik <TimVanMourik@users.noreply.github.com>
2018-08-28 13:29:48 +0200

    Revert "Merge branch 'new-design' into dev"
    This reverts commit d3ca0e76118909e493e6956a7e3254aaa9a91e79, reversing
    changes made to f6042269125d6c32541c3f3b1e3a7867b9783e06.

--- app/giraffe/js/components/slack.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const Slack = () => <div />;
-
-export default Slack;
--- app/giraffe/js/utils/utils.js
+++
@@ -1,8 +0,0 @@
-export function shuffle(o) {
-  for (
-    var j, x, i = o.length;
-    i;
-    j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x
-  );
-  return o;
-}
