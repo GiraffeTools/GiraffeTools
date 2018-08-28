bf0f255a84c6cd6201c7665481ab4b3a06995078
Tim van Mourik <TimVanMourik@users.noreply.github.com>
2018-08-28 13:57:26 +0200

    Revert "Revert "Merge branch 'new-design' into dev""
    This reverts commit 22364dd42b7c7c9bba2f1180eef57f93c73d9943.

---
+++ app/giraffe/js/components/footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Footer = () => (
+  <div className="container fluid mt-3" id="footer">
+    <div className="col text-center">
+      &copy; 2018 Tim van Mourik
+      <br />
+      Imprint | Privacy
+      <br />
+      Designed by Fuchsfabrik
+      <br />
+    </div>
+  </div>
+);
+
+export default Footer;
---
+++ app/giraffe/js/components/thanks.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Thanks = () => (
+  <div id="thanks" className="container fluid mt-3">
+    <h3>With lots of thanks to:</h3>
+    <img src="/static/img/Donders_logo.svg" width="20%" />
+    <img src="/static/img/SIDN_logo.svg" width="20%" />
+  </div>
+);
+
+export default Thanks;
