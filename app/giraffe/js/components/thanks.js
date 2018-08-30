d1549eadc5806da4c9f1864ad6502058d8b8cd53
Tim van Mourik <TimVanMourik@users.noreply.github.com>
2018-08-30 17:50:12 +0200

    add Gitcoin logo

--- app/giraffe/js/components/thanks.js
+++ app/giraffe/js/components/thanks.js
@@ -4,6 +4,7 @@ const Thanks = () => (
   <div id="thanks" className="container-fluid">
     <h3>With lots of thanks to:</h3>
     <img src="/static/img/Donders_logo.svg" width="20%" />
+    <img src="/static/img/Gitcoin_logo.svg" width="20%" />
     <img src="/static/img/SIDN_logo.svg" width="20%" />
   </div>
 );
