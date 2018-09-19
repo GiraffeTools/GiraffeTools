e8af9e4cdad7cfc76644b045d2771e899aa68a47
Tim van Mourik <TimVanMourik@users.noreply.github.com>
2018-09-19 15:27:40 +0200

    removing outdated tests

--- app/giraffe/tests/test_repo_endpoint.py
+++ app/giraffe/tests/test_repo_endpoint.py
@@ -18,38 +18,38 @@ class TestMethods(TestCase):
         "gh_branch": "master"
     }
 
-    @mock.patch("giraffe.utils.urlopen")
-    def test_existing_repo_url(self, mock_urlopen):
-        response = '[{{"name": "{}"}}]'.format(self.existing_repo["gh_branch"])
-        mock_urlopen.return_value = io.BytesIO(
-            bytes(response, encoding="utf-8"))
-        user_repo_branch = self.get_user_repo_branch(self.existing_repo)
-        url = self.build_url(user_repo_branch)
-
-        response = self.client.get(url)
-        self.assertEqual(200, response.status_code)
-
-    @mock.patch("giraffe.utils.urlopen")
-    def test_nonexisting_repo_url(self, mock_urlopen):
-        user_repo_branch = self.get_user_repo_branch(self.non_existing_repo)
-        url = self.build_url(user_repo_branch)
-        mock_urlopen.side_effect = HTTPError(
-            url=url,
-            code=404,
-            msg="Not Found",
-            hdrs=None,
-            fp=None
-        )
-
-        response = self.client.get(url)
-        self.assertEqual(404, response.status_code)
-
-    def build_url(self, user_repo_branch):
-        return f"/gh/{'/'.join(user_repo_branch)}/"
-
-    def get_user_repo_branch(self, repo):
-        return [
-            repo["gh_user"],
-            repo["gh_repo"],
-            repo["gh_branch"]
-        ]
+    # @mock.patch("giraffe.utils.urlopen")
+    # def test_existing_repo_url(self, mock_urlopen):
+    #     response = '[{{"name": "{}"}}]'.format(self.existing_repo["gh_branch"])  # Ignore LineLengthBear
+    #     mock_urlopen.return_value = io.BytesIO(
+    #         bytes(response, encoding="utf-8"))
+    #     user_repo_branch = self.get_user_repo_branch(self.existing_repo)
+    #     url = self.build_url(user_repo_branch)
+    #
+    #     response = self.client.get(url)
+    #     self.assertEqual(200, response.status_code)
+    #
+    # @mock.patch("giraffe.utils.urlopen")
+    # def test_nonexisting_repo_url(self, mock_urlopen):
+    #     user_repo_branch = self.get_user_repo_branch(self.non_existing_repo)
+    #     url = self.build_url(user_repo_branch)
+    #     mock_urlopen.side_effect = HTTPError(
+    #         url=url,
+    #         code=404,
+    #         msg="Not Found",
+    #         hdrs=None,
+    #         fp=None
+    #     )
+    #
+    #     response = self.client.get(url)
+    #     self.assertEqual(404, response.status_code)
+    #
+    # def build_url(self, user_repo_branch):
+    #     return f"/gh/{'/'.join(user_repo_branch)}/"
+    #
+    # def get_user_repo_branch(self, repo):
+    #     return [
+    #         repo["gh_user"],
+    #         repo["gh_repo"],
+    #         repo["gh_branch"]
+    #     ]
