985a61accdc179c6f4f86a3d326c7fc0daaa1316
Tim van Mourik <TimVanMourik@users.noreply.github.com>
2018-09-25 17:48:38 +0200

    appease coala

--- app/app/urls.py
+++ app/app/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 
 from django.conf.urls import include, url
 from django.urls import path
-from django.contrib import admin
+# from django.contrib import admin
 
 import giraffe.views
 import porcupine.views
