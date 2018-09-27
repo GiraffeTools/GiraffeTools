6a45984c37847525b6199ef0986c470647190c9a
Tim van Mourik <TimVanMourik@users.noreply.github.com>
2018-09-27 10:51:58 +0200

    small style update

--- app/giraffe/js/components/people.js
+++ app/giraffe/js/components/people.js
@@ -3,31 +3,35 @@ import React from "react";
 const People = () => (
   <div className="container-fluid text-center" id="people">
     <h2 className="with-lines">THE PEOPLE</h2>
-    <img src="/static/img/tim_artsy_portrait.png" width="20%" />
-    <div className="text-left">
-      <h3>Dr. Tim van Mourik</h3>
-      <br />
-      <img src="/static/img/separator_red.svg" width="20%" />
-      <br />
-      And I automatically create the code for your analysis pipeline. Get
-      started immediately by checking out the example files! It''s so super
-      duper easy.
-      <br />
-      <a href="https://github.com/TimVanMourik" target="_blank">
-        <img src="/static/img/github_icon.svg" />
-      </a>
-      <a
-        href="https://nl.linkedin.com/in/tim-van-mourik-616249a1"
-        target="_blank"
-      >
-        <img src="/static/img/linkedin_icon.svg" />
-      </a>
-      <a href="mailto:timvanmourik@gmail.com" target="_blank">
-        <img src="/static/img/mail_icon.svg" />
-      </a>
-      <a href="https://www.timvanmourik.com" target="_blank">
-        <img src="/static/img/web_icon.svg" />
-      </a>
+    <div className="row justify-content-md-center" id="people">
+      <div className="col col-lg-4 text-center">
+        <img src="/static/img/tim_artsy_portrait.png" width="80%" />
+      </div>
+      <div className="col col-lg-4 text-left people-box">
+        <h3>Dr. Tim van Mourik</h3>
+        <br />
+        <img src="/static/img/separator_red.svg" width="80%" />
+        <br />
+        And I automatically create the code for your analysis pipeline. Get
+        started immediately by checking out the example files! It''s so super
+        duper easy.
+        <br />
+        <a href="https://github.com/TimVanMourik" target="_blank">
+          <img src="/static/img/github_icon.svg" />
+        </a>
+        <a
+          href="https://nl.linkedin.com/in/tim-van-mourik-616249a1"
+          target="_blank"
+        >
+          <img src="/static/img/linkedin_icon.svg" />
+        </a>
+        <a href="mailto:timvanmourik@gmail.com" target="_blank">
+          <img src="/static/img/mail_icon.svg" />
+        </a>
+        <a href="https://www.timvanmourik.com" target="_blank">
+          <img src="/static/img/web_icon.svg" />
+        </a>
+      </div>
     </div>
   </div>
 );
--- app/giraffe/js/components/tools.js
+++ app/giraffe/js/components/tools.js
@@ -2,44 +2,62 @@ import React from "react";
 
 const Porcupine = () => (
   <div className="container-fluid">
-    <img src="/static/img/rec_porcupine_collage.png" id="porcupine-collage" />
-    <div className="col text-left">
-      <h3>Porcupine</h3>
-      <b>POR</b>
-      cupine <b>C</b>
-      reates <b>U</b>r <b>P</b>
-      ipel
-      <b>INE</b>
-      <br />
-      <img src="/static/img/separator_red.svg" width="20%" />
-      <br />
-      With Porcupine, you can visually build your pipeline!
-      <a className="btn btn-lg giraffe-button" role="button" href="/porcupine">
-        Get started!
-      </a>
+    <div className="row">
+      <div className="col">
+        <img
+          src="/static/img/rec_porcupine_collage.png"
+          id="porcupine-collage"
+        />
+      </div>
+      <div className="col text-left">
+        <h3>Porcupine</h3>
+        <b>POR</b>
+        cupine <b>C</b>
+        reates <b>U</b>r <b>P</b>
+        ipel
+        <b>INE</b>
+        <br />
+        <img src="/static/img/separator_red.svg" width="20%" />
+        <br />
+        With Porcupine, you can visually build your pipeline!
+        <a
+          className="btn btn-lg giraffe-button"
+          role="button"
+          href="/porcupine"
+        >
+          Get started!
+        </a>
+      </div>
     </div>
   </div>
 );
 
 const Armadillo = () => (
   <div className="container-fluid">
-    <img src="/static/img/rec_armadillo_collage.png" id="armadillo-collage" />
-    <div className="col text-left">
-      <h3>Armadillo</h3>
-      <br />
-      <img src="/static/img/separator_red.svg" width="20%" />
-      <br />
-      <b>A</b>
-      ugmented <b>R</b>
-      eality [madillo] This web app creates 3D Augmented Reality images from the
-      Neurovault database
-      <a
-        className="btn btn-lg giraffe-button"
-        role="button"
-        href="https://armadillobrain.app"
-      >
-        Get started!
-      </a>
+    <div className="row">
+      <div className="col">
+        <img
+          src="/static/img/rec_armadillo_collage.png"
+          id="armadillo-collage"
+        />
+      </div>
+      <div className="col text-left">
+        <h3>Armadillo</h3>
+        <br />
+        <img src="/static/img/separator_red.svg" width="20%" />
+        <br />
+        <b>A</b>
+        ugmented <b>R</b>
+        eality [madillo] This web app creates 3D Augmented Reality images from
+        the Neurovault database
+        <a
+          className="btn btn-lg giraffe-button"
+          role="button"
+          href="https://armadillobrain.app"
+        >
+          Get started!
+        </a>
+      </div>
     </div>
   </div>
 );
