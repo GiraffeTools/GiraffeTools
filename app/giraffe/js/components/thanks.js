18c75aaa90b5de4251fd37951390148cfbab6397
Tim van Mourik <TimVanMourik@users.noreply.github.com>
2018-10-02 15:15:32 +0200

    package update

--- app/giraffe/js/components/thanks.js
+++ app/giraffe/js/components/thanks.js
@@ -3,9 +3,9 @@ import React from "react";
 const Thanks = () => (
   <div id="thanks" className="container-fluid">
     <h3>With lots of thanks to:</h3>
-    <img src="/static/img/donders_logo.svg" width="20%" />
-    <img src="/static/img/gitcoin_logo.svg" width="20%" />
-    <img src="/static/img/sidnfonds_logo.svg" width="20%" />
+    <img src="/static/img/donders_logo.svg" className="thanks-logo" />
+    <img src="/static/img/gitcoin_logo.svg" className="thanks-logo" />
+    <img src="/static/img/sidnfonds_logo.svg" className="thanks-logo" />
   </div>
 );
 
