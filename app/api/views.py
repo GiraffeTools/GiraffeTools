87604cb45cda886edafe94b88494b8b2d0e12333
Tim van Mourik <TimVanMourik@users.noreply.github.com>
2018-11-08 17:34:49 +0100

    access token fix

--- app/api/views.py
+++ app/api/views.py
@@ -41,8 +41,11 @@ def push_to_github(request):
     # Clearly it should only do this on your own repo when you're logged in...
     logged_in_user = logged_in(request)
     github_user = json.loads(logged_in_user.content)
+    print(github_user["github_name"])
+    print("access_token" in github_user and is_github_token_valid(
+        github_user["access_token"]))
     if (github_user["github_name"] != user or
-            not is_github_token_valid(github_user.access_token)):
+            not is_github_token_valid(github_user["access_token"])):
         response = {}
         return HttpResponse(response, content_type="application/json")
 
