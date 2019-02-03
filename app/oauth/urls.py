ee6a88b01ced1c6d18b74a9a0a2153471bd9db5b
Tim van Mourik <TimVanMourik@users.noreply.github.com>
2019-02-03 20:20:56 +0100

    callback implemented

--- app/oauth/urls.py
+++ app/oauth/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import login, logout
+from .views import login, logout, callback
 
 app_name = "oauth"
 
 urlpatterns = [
+    path("callback/", callback, name="callback"),
     path("login/", login, name="login"),
     path("logout/", logout, name="logout"),
 ]
--- app/oauth/views.py
+++ app/oauth/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import redirect
 from django.conf import settings
+from django.http import Http404
 
 def login(request):
     return redirect("social:begin", backend="github")
@@ -7,3 +8,11 @@ def login(request):
 
 def logout(request):
     return redirect(settings.LOGOUT_URL, backend="github")
+
+
+def callback(request):
+    redirect_uri = request.GET.get("redirect_uri")
+    if not redirect_uri:
+        raise Http404
+
+    return redirect(redirect_uri)
