8d7ee13282088acf339f0e0fbe46773e4880ed96
Tim van Mourik <TimVanMourik@users.noreply.github.com>
2019-02-05 11:10:15 +0100

    move logout action to before actual logout

--- app/oauth/views.py
+++ app/oauth/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import redirect
-from django.http import Http404
 from django.contrib.auth import logout
 
 from giraffe.utils import log_action
@@ -10,15 +9,12 @@ def login(request):
 
 
 def logout_view(request):
-    logout(request)
     log_action(request, "Logout")
+    logout(request)
     # TODO make logout screen
     return redirect("/")
 
 
 def callback(request):
-    redirect_uri = request.GET.get("redirect_uri")
-    if not redirect_uri:
-        raise Http404
-
+    redirect_uri = request.GET.get("redirect_uri", "/")
     return redirect(redirect_uri)
