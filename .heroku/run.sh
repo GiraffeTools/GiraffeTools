3b0b41085eb82d09d08f320cf9f0a6382fd9c28f
Tim van Mourik <timvanmourik@gmail.com>
2019-04-24 12:33:53 +0200

    copy files without modification

--- .heroku/run.sh
+++ .heroku/run.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-apt-get update && apt-get install jq
 
-PACKAGE=$(jq . package.json)
-SCRIPTS=$(jq . heroku-scripts.json)
+# apt-get update && apt-get install jq
+#
+# PACKAGE=$(jq . package.json)
+# SCRIPTS=$(jq . heroku-scripts.json)
+#
+# JSON_STRING=$( jq -n \
+#   "${PACKAGE}, ${SCRIPTS}" \
+# )
+#
+# echo $JSON_STRING > package.json
 
-JSON_STRING=$( jq -n \
-  "${PACKAGE}, ${SCRIPTS}" \
-)
-
-echo $JSON_STRING > package.json
+cp ./frontend/package.json ./package.json
 cp ./frontend/package-lock.json ./package-lock.json
