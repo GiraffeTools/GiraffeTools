892abc31b4ce2a93222868b67a49734a34d1779a
Tim van Mourik <timvanmourik@gmail.com>
2019-09-04 11:46:02 +0200

    Update contributors.js

--- frontend/giraffe/components/contributors.js
+++ frontend/giraffe/components/contributors.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Radium from 'radium';
 
 import {addTokenToQuery} from '../utils/auth';
@@ -6,99 +6,72 @@ import {shuffle} from '../utils/utils';
 import Contributor from './contributor';
 import styles from '../styles/contributors.js';
 
-class Contributors extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      contributors: null,
-      selectedContributors: null,
-    };
-    this.selectContributors = this.selectContributors.bind(this);
-  }
 
-  async componentDidMount() {
-    const url = await addTokenToQuery(
-        new URL(
-            'https://api.github.com/repos/TimVanMourik/GiraffeTools/contributors'
-        )
-    );
-    const contributors = await (await fetch(url.href)).json();
-    if (!contributors.length) return;
-    this.setState({
-      contributors: contributors,
-      selectContributors: shuffle(
-          // eslint-disable-next-line
-          Array.apply(null, {length: contributors.length}).map(
-              Number.call,
-              Number
-          )
-      ).slice(0, 9),
-    });
-  }
-
-  selectContributors(direction) {
-    const {contributors} = this.state;
-    // #TODO implement pagination
-    switch (direction) {
-      case 'next':
-        break;
-      case 'previous':
-        break;
-    }
-    // Just reshuffle them randomly for now
-    this.setState({
-      selectContributors: shuffle(
-          // eslint-disable-next-line
-          Array.apply(null, {length: contributors.length}).map(
-              Number.call,
-              Number
-          )
-      ).slice(0, 9),
-    });
-  }
-
-  render() {
-    const {contributors, selectContributors} = this.state;
-
-    return (
-      <div style={[styles.contributors]}>
-        <div className="col-6 border-right">
-          <h3>&amp; all the brave contributors</h3>
-          {contributors && (
-            <div className="d-flex">
-              <img
-                src="/static/img/arrow_left.svg"
-                style={[styles.contributorArrow]}
-                onClick={() => this.selectContributors('previous')}
-              />
-              <div style={[styles.contributorList]}>
-                {contributors &&
-                  contributors
-                      .filter((item, index) =>
-                        selectContributors.includes(index))
-                      .map((contributor) => (
-                        <Contributor key={contributor.id} {...contributor} />
-                      ))}
-              </div>
-              <img
-                style={[styles.contributorArrow]}
-                src="/static/img/arrow_right.svg"
-                onClick={() => this.selectContributors('next')}
-              />
-            </div>
-          )}
-        </div>
-        <div className="col-6 justify-content-center d-flex row">
-          <h3 style={[styles.gitcoinText]}>
-            Most contributors have contributed to this Open Source project with
-            Gitcoin. If you want to help grow this platform, you can donate or
-            fund projects and issues directly!
-          </h3>
-          <img src="/static/img/eth-qr.svg" style={[styles.ethQrCode]} />
-        </div>
-      </div>
-    );
-  }
+async function loadContributors(setContributors) {
+  const url = await addTokenToQuery(
+      new URL(
+          'https://api.github.com/repos/TimVanMourik/GiraffeTools/contributors'
+      )
+  );
+  const contributors = (await (await fetch(url.href)).json())
+    .filter(contributor => !contributor.login.includes("[bot]"));
+  
+  setContributors(shuffle(contributors));
 }
 
-export default Radium(Contributors);
+function mount(setContributors) {
+  loadContributors(setContributors)
+}
+
+const modulo = (n, m) => ((n % m) + m) % m
+
+const NUMBER_OF_CONTRIBUTORS_SHOWN = 9;
+const Contributors = () => {
+  useEffect(() => mount(setContributors), []);
+
+  const [contributors, setContributors] = useState([]);
+  const [selection, setSelection] = useState(0);
+
+  const indices = Array.from(
+    new Array(NUMBER_OF_CONTRIBUTORS_SHOWN), (x,i) => modulo(i + selection, contributors.length)
+  );
+  const visibleContributors = contributors && contributors
+  .filter((item, index) => indices.includes(index))
+  .map((contributor) => (
+    <Contributor key={contributor.id} {...contributor} />
+  ));
+
+  return (
+    <div style={styles.contributors}>
+      <div className="col-6 border-right">
+        <h3>&amp; all the brave contributors</h3>
+        {contributors && (
+          <div className="d-flex">
+            <img
+              src="/static/img/arrow_left.svg"
+              style={styles.contributorArrow}
+              onClick={() => setSelection(selection + NUMBER_OF_CONTRIBUTORS_SHOWN)}
+            />
+            <div style={styles.contributorList}>
+              {visibleContributors}
+            </div>
+            <img
+              style={styles.contributorArrow}
+              src="/static/img/arrow_right.svg"
+              onClick={() => setSelection(selection - NUMBER_OF_CONTRIBUTORS_SHOWN)}
+            />
+          </div>
+        )}
+      </div>
+      <div className="col-6 justify-content-center d-flex row">
+        <h3 style={styles.gitcoinText}>
+          Most contributors have contributed to this Open Source project with
+          Gitcoin. If you want to help grow this platform, you can donate or
+          fund projects and issues directly!
+        </h3>
+        <img src="/static/img/eth-qr.svg" style={styles.ethQrCode} />
+      </div>
+    </div>
+  );
+}
+export default Contributors;
